182747b58747a5c54b544d936503e3b67e57abdc
Steven Than <steventhan11@gmail.com>
2017-03-17 22:57:18 -0400

    removed tests code

--- src/TweetBot.java
+++ src/TweetBot.java
@@ -7,20 +7,6 @@ public class TweetBot {
     public static void main(String[] args) throws EmptyLinkedListException, TwitterException {
         new TweetThread().start();
         new StreamThread().start();
-
-//        TweetQueue queue = new TweetQueue();
-//        Twitter twitter = TwitterFactory.getSingleton();
-//        List<Status> statuses = twitter.getHomeTimeline();
-//        for (Status status : statuses) {
-//            queue.enqueue(status);
-//        }
-//        System.out.println(queue.isEmpty());
-//        System.out.println(queue.size());
-//        System.out.println(queue.dequeue().getText());
-//        System.out.println(queue.size());
-//        System.out.println(queue.dequeue().getText());
-//        System.out.println(queue.size());
-//        System.out.println(queue.isEmpty());
     }
 
 }
